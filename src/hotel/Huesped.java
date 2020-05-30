c1e3a0fd5afd6febefe082b34a4b040b74f1c43a
GimenezTomas <63616281+GimenezTomas@users.noreply.github.com>
2020-05-30 00:34:23 -0300

    Super clase persona

--- src/hotel/Huesped.java
+++ src/hotel/Huesped.java
@@ -1,12 +1,12 @@
 package hotel;
 
-public class Huesped {
-    private int edad;
-    private int dni;
-    private String apellido;
-    private String nombre;
+public class Huesped extends Persona{
+
     private int numeroDeHuesped;
     public static int count=1;
+
+    //GETTERS && SETTERS
+
     public int getNumeroDeHuesped() {
         return numeroDeHuesped;
     }
@@ -15,45 +15,9 @@ public class Huesped {
         this.numeroDeHuesped = numeroDeHuesped;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getDni() {
-        return dni;
-    }
-
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
     public Huesped(String apellido, String nombre, int edad, int dni)
     {
-        this.apellido=apellido;
-        this.dni=dni;
-        this.nombre=nombre;
-        this.edad=edad;
+        super(apellido, nombre, edad, dni);
         numeroDeHuesped=count++;
     }
-
-
 }
